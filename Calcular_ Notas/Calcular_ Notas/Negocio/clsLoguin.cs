60f65a94f5653c9dd09605522ec924bee056ac4b
ha0372 <69734226+ha0372@users.noreply.github.com>
2020-10-11 14:38:31 -0600

    Metodos

--- Calcular_ Notas/Calcular_ Notas/Negocio/clsLoguin.cs
+++ Calcular_ Notas/Calcular_ Notas/Negocio/clsLoguin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calcular__Notas.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,40 @@ namespace Calcular__Notas.Negocio
 {
     class clsLoguin
     {
+        public Double cal1(Loguin log1)
+        {
+            Double total1 = (log1.Lab1 * 0.4) + (log1.Par1 * 0.6);
 
+            return total1;
+        }
+
+        public Double cal2(Loguin log2)
+        {
+            Double total2 = (log2.Lab1 * 0.4) + (log2.Par1 * 0.6);
+
+            return total2;
+
+        }
+        public Double cal3(Loguin log3)
+        {
+            Double total3 = (log3.Lab1 * 0.4) + (log3.Par1 * 0.6);
+
+            return total3;
+        }
+        public Double calF(Loguin logF)
+        {
+            Double totalF = logF.Nota1 + logF.Nota2 + logF.Nota3;
+
+            return totalF;
+        }
+        
+        public Boolean apro(Loguin logA)
+        {
+            Double result = (logA.Nota1 + logA.Nota2 + logA.Nota3) / 3;
+
+            return result < 6.0 ;
+        }
+      
 
     }
 }
