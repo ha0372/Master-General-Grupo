5d74dc1d15d7746eb2fe48e238bb0714a7bacba6
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2020-10-11 08:33:48 -0600

    Actualizacion

--- Calcular_ Notas/Calcular_ Notas/Dominio/Loguin.cs
+++ Calcular_ Notas/Calcular_ Notas/Dominio/Loguin.cs
@@ -6,5 +6,14 @@ namespace Calcular__Notas.Dominio
 {
     class Loguin
     {
+        private double lab1;
+        private double par1;
+        private double lab2;
+        private double par2;
+        private double lab3;
+        private double par3;
+        private double nota1;
+        private double nota2;
+        private double nota3;
     }
 }
