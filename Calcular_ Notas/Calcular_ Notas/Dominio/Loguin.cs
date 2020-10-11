406aef656a57fecfc51a0cc3dd612e175009688f
ha0372 <69734226+ha0372@users.noreply.github.com>
2020-10-11 08:57:05 -0600

    get y set

--- Calcular_ Notas/Calcular_ Notas/Dominio/Loguin.cs
+++ Calcular_ Notas/Calcular_ Notas/Dominio/Loguin.cs
@@ -15,5 +15,15 @@ namespace Calcular__Notas.Dominio
         private double nota1;
         private double nota2;
         private double nota3;
+
+        public double Lab1 { get => lab1; set => lab1 = value; }
+        public double Par1 { get => par1; set => par1 = value; }
+        public double Lab2 { get => lab2; set => lab2 = value; }
+        public double Par2 { get => par2; set => par2 = value; }
+        public double Lab3 { get => lab3; set => lab3 = value; }
+        public double Par3 { get => par3; set => par3 = value; }
+        public double Nota1 { get => nota1; set => nota1 = value; }
+        public double Nota2 { get => nota2; set => nota2 = value; }
+        public double Nota3 { get => nota3; set => nota3 = value; }
     }
 }
